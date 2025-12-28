64a421f08bb0e4be8920e0c65141b50992734e58
uppuluriashok <uppuluri.ashok@gmail.com>
2025-12-28 14:28:08 +0530

    "Fix CORS + OPTIONS for Ionic"
    "Fix CORS + OPTIONS for Ionic"

--- src/main/java/com/example/auth/controller/AuthController.java
+++ src/main/java/com/example/auth/controller/AuthController.java
@@ -32,7 +32,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4000", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:8100", allowCredentials = "true")
 public class AuthController {
     @Autowired
    private AuthService authService;
